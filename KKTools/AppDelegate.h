7c58cc04e7d4fb09509009ae72e40b4e9ac1cb9f
zving <zving@bogon>
2017-06-29 14:26:43 +0800

    Initial Commit

---
+++ KKTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKTools
+//
+//  Created by zving on 2017/6/29.
+//  Copyright © 2017年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKTools
+//
+//  Created by zving on 2017/6/29.
+//  Copyright © 2017年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
